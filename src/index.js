53018e209c0edfe2fa8835b97d146dac2f60b75c
Evan McClaugherty <emcclaugherty@cobank.com>
2018-11-20 17:24:39 -0700

    add app component, call to geo service, render in view

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    return <div>Hi there!</div>
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            latitude: 1,
+            longitude: 2,
+            hasGeoError: false,
+            errorMessage: "Error retrieving coordinates"
+        }
+        this.getCoordinates();
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.hasGeoError &&
+                    <h2>{this.state.errorMessage}</h2>
+                }
+                <p>latitude: {this.state.latitude}</p>
+                <p>longitude: {this.state.longitude}</p>
+                <SeasonDisplay />
+            </div>
+        );
+    }
+
+    getCoordinates() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    hasGeoError: false,
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            },
+            (error) => {
+                this.setState({
+                    hasGeoError: true,
+                    errorMessage: error.message
+                });
+            }
+        );
+    }
 }
 
 ReactDOM.render(

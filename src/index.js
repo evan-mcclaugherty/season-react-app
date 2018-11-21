aec13a8039720e6a811ff7ac5d2cab50446150ac
Evan McClaugherty <evan.mcclaugherty@gmail.com>
2018-11-21 07:34:15 -0700

    update index

--- src/index.js
+++ src/index.js
@@ -4,46 +4,61 @@ import SeasonDisplay from './SeasonDisplay';
 
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            latitude: 1,
-            longitude: 2,
-            hasGeoError: false,
-            errorMessage: "Error retrieving coordinates"
-        }
-        this.getCoordinates();
+    //babel turns this into a constructor so this is a shortcut
+    state = {
+        latitude: false,
+        errorMessage: false
     }
 
     render() {
         return (
             <div>
-                {this.state.hasGeoError &&
+                {this.state.errorMessage &&
                     <h2>{this.state.errorMessage}</h2>
                 }
-                <p>latitude: {this.state.latitude}</p>
-                <p>longitude: {this.state.longitude}</p>
+                {this.state.latitude &&
+                    <p>latitude: {this.state.latitude}</p>
+                }
                 <SeasonDisplay />
             </div>
         );
     }
 
-    getCoordinates() {
+    //good for data initialization
+    //Only gets called 1 time
+    componentDidMount() {
+        console.log("Component DID mount")
         window.navigator.geolocation.getCurrentPosition(
             (position) => {
                 this.setState({
-                    hasGeoError: false,
                     latitude: position.coords.latitude,
-                    longitude: position.coords.longitude
+                    errorMessage: false
                 });
             },
             (error) => {
                 this.setState({
-                    hasGeoError: true,
+                    latitude: false,
                     errorMessage: error.message
                 });
             }
         );
+    }
+
+    //More data loading for when state/props change
+    componentDidUpdate() {
+        console.log("Component Did Update")
+    }
+
+    //cleanup method for cleanup.
+    componentWillUnmount() {
+        console.log("Component will unmount")
+    }
+
+    componentWillUpdate() {
+        console.log("Component WILL update")
+    }
+    componentDidCatch() {
+        console.log("Component DID catch")
     }
 }
 
